0eb4d2db7fc5e2f0e5193c092ae0bd9c8176eb70
drbarq <J.Tustin@gmail.com>
2019-04-25 13:44:14 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,51 @@
 
 
+
+require "pry"
+
+
+class Book
+  @@all_books = []
+
+
+  def initialize(title)
+    @title = title
+    @@all_books << @title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(numberofpages)
+    @pages = numberofpages
+  end
+
+  def page_count
+    @pages
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+
+# Book.new("And Then There Were None")
